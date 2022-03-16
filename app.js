0656a10abd6d17832d95592010f5e95c2a58cc05
Ingosi <bingosi@leisdstudent.ws>
2022-03-16 14:53:24 -0500

    have all aspects of code needed, just not functioning, changes need to be made

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-player = 0
-computer = 0
+player = numran1
+computer = numran2
 
 do {
     alert("The point of this game is to draw random numbers from 1 to 10, to see who gets closer to 20.")
+    alert("I'll go first.")
 
     do {
-    var numran = Math.floor(Math.random() * 10)
-    computer ++
-    alert(`Ok, my number is ${numran} and my total is ${computer}`)
+    var numran1 = Math.floor(Math.random() * 10)
+    computer++
+    alert(`Ok, my number is ${numran1} and my total is ${numran1}`)
     } while(computer >= 16)
 
     do {
     var drawnum = prompt("Do you want to draw a number? If so enter `y`")
         if (drawnum = "y") {
-            var numran = Math.floor(Math.random() * 10)
-            player ++
-            alert(`Your number is ${numran} and your total is ${player}`)
-            break;
+            var numran2 = Math.floor(Math.random() * 10)
+            player++
+            alert(`Your number is ${numran2} and your total is ${numran2}`)
     }
     } while(player < 20)
 
-} while((computer > 20) || (player > 20))
+} while((computer < 20) || (player < 20))
 
 if (player > 20) {
     alert("Sorry, you lose!")
