cc70f36a7e4dde5f1b100467831fad075638b01a
Ingosi <bingosi@leisdstudent.ws>
2022-03-17 14:16:45 -0500

    didn't add any changes just changed the order the code is layed out

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-player = numran1
-computer = numran2
+
+player = 0
+computer = 0
+
+alert("The point of this game is to draw random numbers from 1 to 10, to see who gets closer to 20.")
+alert("I'll go first.")
 
 do {
-    alert("The point of this game is to draw random numbers from 1 to 10, to see who gets closer to 20.")
-    alert("I'll go first.")
-
+    
     do {
     var numran1 = Math.floor(Math.random() * 10)
     computer++
@@ -19,7 +21,7 @@ do {
             alert(`Your number is ${numran2} and your total is ${numran2}`)
     }
     } while(player < 20)
-
+    
 } while((computer < 20) || (player < 20))
 
 if (player > 20) {
