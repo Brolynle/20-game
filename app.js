4cad93ba085143f4cdf5c522f7c350955d8490dd
Ingosi <bingosi@leisdstudent.ws>
2022-03-18 14:25:20 -0500

    added a total to computer and player numbers

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 
+player = 0
+computer = 0
+
 alert("The point of this game is to draw random numbers from 1 to 10, to see who gets closer to 20.")
 alert("I'll go first.")
 
 do {
     do {
     var numran1 = Math.floor(Math.random() * 10)
-    var computer = numran1
-    computer1 = numran + numran
-
-    alert(`Ok, my number is ${numran1} and my total is ${computer1}`)
-    } while(computer1 >= 16)
+    alert(`Ok, my number is ${numran1}`)
+    comtotal = numran1 + numran1 
+    } while(comtotal >= 16)
 
     do {
     var drawnum = prompt("Do you want to draw a number? If so enter `y`")
         if (drawnum = "y") {
             var numran2 = Math.floor(Math.random() * 10)
-            player++
-            alert(`Your number is ${numran2} and your total is ${numran2}`)
+            playtotal = numran2 + numran2
+            alert(`Your number is ${numran2} and your total is ${playtotal}`)
     }
     } while(player < 20)
+
+    if (player > 20) {
+        alert("Sorry, you lose!")
+        computer++
+    }
+    
+    if (computer > 20) {
+        alert("Good job, you won!")
+        player++
+    }
     
 } while((computer < 20) || (player < 20))
 
 
-if (player > 20) {
-    alert("Sorry, you lose!")
-}
 
-if (computer > 20) {
-    alert("Good job, you won!")
-}
+
